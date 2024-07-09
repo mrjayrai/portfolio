44df8ee74d9443b8a8299ec628155aafcfed5130
mrjayrai <raijai127@gmail.com>
2024-07-09 21:03:14 +0530

    Added Styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden
+    mx-auto px-5 sm:px-10">
+      <div className="max-w-7xl w-full text-white text-center">
+        Hello World
+      </div>
     </main>
   );
 }
