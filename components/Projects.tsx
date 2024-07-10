0be7033769f787ac63edde1ab3e4f3f80ecdc61b
mrjayrai <raijai127@gmail.com>
2024-07-11 00:37:45 +0530

    .

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -21,7 +21,7 @@ const RecentProjects = () => {
           >
             <PinContainer
               title={item.link}
-              href="https://twitter.com/mannupaaji"
+              href={item.link}
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
